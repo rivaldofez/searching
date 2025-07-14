6c18991853546a13ef34320e4a306ed848621099
Rivaldo Fernandes <rivaldofez@gmail.com>
2025-07-14 11:49:40 +0700

    feat: setup base project and remove main storyboard

---
+++ app/Searching/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Searching
+//
+//  Created by Rivaldo Fernandes on 14/07/25.
+//
+
+import UIKit
+import Combine
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
